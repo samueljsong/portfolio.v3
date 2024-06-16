3893cfcdc060e5ab53f18e8027ffea5d4a469758
samueljsong <semuall0108@gmail.com>
2024-06-15 21:42:21 -0700

    working on about page

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * Splits text elements and makes a span element for each letter.
+ * @param {*} selector
+ */
 function splitTextIntoSpans(selector) {
     let element = document.querySelector(selector);
     if (element) {
@@ -10,25 +14,39 @@ function splitTextIntoSpans(selector) {
     }
 }
 
+const setLineLength = (lines) => {
+    let multiplyVariable = 1;
+    let width = 100;
+    lines.forEach((line) => {
+        line.style.width = `${width * multiplyVariable}%`;
+        multiplyVariable *= 0.6;
+    });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+    // Gsap constants
+    gsap.registerPlugin(ScrollTrigger);
+    const t1 = gsap.timeline();
+    const lines = document.querySelectorAll(".line");
+    const lineTimeline = gsap.timeline({ repeat: -1, repeatDelay: 0 });
+
     splitTextIntoSpans(".landing-title");
     splitTextIntoSpans(".info-text-container");
 
     let letters = document.querySelectorAll(".landing-title > span");
     let infoLetters = document.querySelectorAll(".info-text-container > span");
-    console.log(letters);
 
-    gsap.registerPlugin(ScrollTrigger);
-
-    const t1 = gsap.timeline();
-
-    t1.from(letters, {
-        y: 600,
-        ease: "power3.inOut",
-        stagger: 0.075,
-        delay: 0.2,
-        duration: 1.5,
-    });
+    t1.from(
+        letters,
+        {
+            y: 600,
+            ease: "power3.inOut",
+            stagger: 0.075,
+            delay: 0.2,
+            duration: 1.5,
+        },
+        0.25
+    );
 
     t1.from(
         infoLetters,
@@ -37,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
             ease: "power3.inOut",
             duration: 1,
         },
-        1.5
+        1.75
     );
 
     t1.from(
@@ -47,13 +65,78 @@ document.addEventListener("DOMContentLoaded", () => {
             opacity: 0,
             ease: "power3.inOut",
             duration: 1,
+            pointerEvents: "none",
         },
-        1.5
+        1.75
     );
 
-    t1.from(".copyright", {
-        opacity: 0,
-        ease: "power3.inOut",
-        duration: 1,
+    t1.from(
+        ".copyright",
+        {
+            opacity: 0,
+            ease: "power3.inOut",
+            duration: 1,
+        },
+        1.75
+    );
+
+    // Enter portfolio animations
+    const t2 = gsap.timeline();
+    const enterButton = document.querySelector(".landing-btn");
+    enterButton.addEventListener("click", () => {
+        let elements = [".landing", ".copyright"];
+
+        t2.to(
+            elements,
+            {
+                opacity: 0,
+                duration: 0.5,
+                ease: "power3.out",
+                display: "none",
+            },
+            0
+        );
+
+        t2.to(".main-content", {
+            display: "block",
+        });
+
+        setLineLength(lines);
+
+        t2.to(
+            lines,
+            {
+                opacity: 1,
+                stagger: 0.08,
+                duration: 0.1,
+                ease: "power2.in",
+                onComplete: () => {
+                    lineTimeline.to(
+                        lines,
+                        {
+                            opacity: 0,
+                            stagger: 0.08,
+                            duration: 0.5,
+                            ease: "power3.in",
+                        },
+                        1
+                    );
+
+                    lineTimeline.to(
+                        lines,
+                        {
+                            opacity: 1,
+                            stagger: 0.08,
+                            duration: 0.5,
+                            ease: "power3.in",
+                        },
+                        1.5
+                    );
+                },
+            },
+            1.15
+        );
+
+        lineTimeline.delay(1);
     });
 });
---
+++ public/js/mainContent.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const t1 = gsap.timeline();
+});
